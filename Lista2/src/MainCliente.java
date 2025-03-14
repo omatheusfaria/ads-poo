437df6858aeb8687c217d10f3fa6c8b47f6a1ebf
omatheusfaria <lampersmatheusdk@gmail.com>
2025-03-14 20:17:57 -0300

    Adicionada Classa Cliente e MainCliente

---
+++ Lista2/src/MainCliente.java
@@ -0,0 +1,11 @@
+import exe1.Cliente;
+
+public class MainCliente {
+    public static void main(String[] args){
+        Cliente obj1 = new Cliente();
+        obj1.setNumConta("123456-7");
+        obj1.setNumAgencia("1234-5");
+        obj1.setNome("Matheus");
+        obj1.setSaldo(0);
+    }
+}
--- Lista2/src/exe1/Cliente.java
+++ Lista2/src/exe1/Cliente.java
@@ -1,10 +1,56 @@
 package exe1;
 
 public class Cliente {
-    private String numConta;
-    private String numAgencia;
-    private String nome;
+    private String nome, numConta, numAgencia;
     private float saldo;
 
+    public void setNumConta(String numConta){
+        if(numConta.length() == 8){
+            if (numConta.charAt(6) == '-'){
+                this.numConta = numConta;
+            }
+            else System.out.println("Problema no DV");
+        }
+        else System.out.println("Problema no tamanho");
+    }
 
+    public void setNumAgencia(String numAgencia){
+        if (numAgencia.length() == 6){
+            if (numAgencia.charAt(4) == '-'){
+                this.numAgencia = numAgencia;
+            }
+            else System.out.println("Problema no DV");
+        }
+        else System.out.println("Problema no tamanho");
+    }
+
+    public void setNome (String nome){
+        if (nome.length() <= 30){
+            this.nome = nome;
+        }
+        else System.out.println("Nome invalido");
+    }
+
+    public void setSaldo(float saldo){
+        if (saldo >= 0){
+            this.saldo = saldo;
+        }
+        else System.out.println("Saldo negativo");
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getNumConta(){
+        return this.numConta;
+    }
+
+    public String getNumAgencia(){
+        return this.numAgencia;
+    }
+
+    public float getSaldo() {
+        return this.saldo;
+    }
 }
