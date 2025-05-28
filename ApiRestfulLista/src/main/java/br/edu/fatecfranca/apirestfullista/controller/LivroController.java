5dfe1fc5b003e5ec92b4a5101c48cd127f310af9
Matheus Faria <lampersmatheusdk@gmail.com>
2025-05-28 14:42:02 -0300

    Finalizãção CRUD ApiRestFulLista

--- ApiRestfulLista/src/main/java/br/edu/fatecfranca/apirestfullista/controller/LivroController.java
+++ ApiRestfulLista/src/main/java/br/edu/fatecfranca/apirestfullista/controller/LivroController.java
@@ -6,18 +6,49 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController //Indica que a classe recebe e responde requisição REST
-@RequestMapping("/livro") //Recebe e responde o endpoint /livro
+@RestController // indica que a classe recebe e responde requisição REST
+@RequestMapping("/livro") // recebe e responde o endpoint /livro
 public class LivroController {
-    //banco de dados em memoria
+    // endpoint http://localhost:8080/livro
+
+    // banco de dados em memória - List
     List<Livro> livros = new ArrayList<>();
-    @GetMapping //Método responde requisição GET
+
+    @GetMapping // método responde requisição GET
     public List<Livro> getLivros(){
-        return this.livros;//Retorna vetor com os livros
+        return this.livros; // retorna o vetor com os livros
     }
-    @PostMapping//Método responde requisição POST
+    @PostMapping // método responde requisição POST
     public Livro addLivro(@RequestBody Livro livro){
-        this.livros.add(livro);
+        this.livros.add(livro); // adiciona livro no vetor
         return livro;
     }
+    @DeleteMapping("/{id}")
+    public String deleteLivro(@PathVariable Long id){
+        // vetor tem um método chamado removeIf
+        // que remove caso o livro seja encontrado
+        // removeIf percorre o vetor - livro representa cada livro no vetor
+        boolean resp = this.livros.removeIf(livro -> livro.getId() == id);
+        if(resp){
+            return "Livro removido com sucesso!";
+        }
+        else {
+            return "Livro nao encontrado";
+        }
+    }
+    @PutMapping("/{id}")
+    public Livro updateLivro(@PathVariable Long id, @RequestBody Livro novo){
+        // percorre o vetor
+        for(Livro livro : this.livros){
+            // se o id do livro no vetor = id do frontend
+            if(livro.getId() == id){
+                livro.setTitulo(novo.getTitulo());
+                livro.setAutor(novo.getAutor());
+                livro.setEditora(novo.getEditora());
+                return livro; // retorna livro atualizado
+            }
+        }
+        return null;
+    }
+
 }
