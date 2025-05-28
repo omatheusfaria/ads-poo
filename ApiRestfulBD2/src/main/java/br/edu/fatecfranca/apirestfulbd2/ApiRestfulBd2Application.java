4df1ee65f8f43b814fc08e0e5217a01d6429f0fe
Matheus Faria <lampersmatheusdk@gmail.com>
2025-05-28 15:40:45 -0300

    ApiRestFulBD2

---
+++ ApiRestfulBD2/src/main/java/br/edu/fatecfranca/apirestfulbd2/ApiRestfulBd2Application.java
@@ -0,0 +1,13 @@
+package br.edu.fatecfranca.apirestfulbd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulBd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulBd2Application.class, args);
+	}
+
+}
---
+++ ApiRestfulBD2/src/test/java/br/edu/fatecfranca/apirestfulbd2/ApiRestfulBd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecfranca.apirestfulbd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulBd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
